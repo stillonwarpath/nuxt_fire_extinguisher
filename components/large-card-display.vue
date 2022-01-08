12be2d6b096795e58c1e3c45cacccbdc57e0cad6
francisco <fr.betancourt@live.com>
2022-01-08 17:46:11 -0300

    Added header and snippet of large card display.

--- components/large-card-display.vue
+++ components/large-card-display.vue
@@ -1,6 +1,11 @@
 <template>
-  <div>
-    Hi!
+  <div class="container">
+    <h4 class="header">
+      Top-rated Fire Extinguisher in Niagara
+    </h4>
+    <p class="snippet">
+      User say these are the best fire extinguisher to rent if your house is burning down.
+    </p>
   </div>
 </template>
 
