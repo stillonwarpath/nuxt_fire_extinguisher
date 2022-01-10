0472f699172bde07cb96b4a8a117cb2312a0af96
francisco <fr.betancourt@live.com>
2022-01-09 23:33:56 -0300

    Added small card display styles

--- components/SmallCardDisplay.vue
+++ components/SmallCardDisplay.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="container">
-        <h3 class="header"></h3>
-        <div class="card-container"></div>
-    </div>
+  <div class="container">
+    <h3 class="header"></h3>
+    <div class="card-container"></div>
+  </div>
 </template>
 
 <script>
@@ -10,6 +10,18 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+    .container {
+        padding: 2rem 0;
+    }
+    .header {
+        font-weight: 700;
+        font-size: 1.5rem;
+        margin-bottom: 2rem;
+    }
+    .card-container {
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap;
+    }
 </style>
