e2797b52d5a24706d14ece945caabeb819e6e003
francisco <fr.betancourt@live.com>
2022-01-14 17:01:50 -0300

    Fixing code spacing.

--- pages/products/_id.vue
+++ pages/products/_id.vue
@@ -1,39 +1,39 @@
 <template>
-      <div v-if="product" class="container py-5">
-        <div class="hero-container">
-            <img :src="require(`@/assets/images/${product.image}`)" alt="" class="image">
-            <div class="info-box">
-                <h1>{{ product.title }}</h1>
-                <p class="snippet">{{ product.snippet }}</p>
-            </div>
-        </div>
-        <div class="whats-included-container">
-            <div class="included-container">
-                <h6>Super Effective</h6>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, dolorem.</p>
-            </div>
-            <div class="included-container">
-                <h6>Clean & Tidy</h6>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, dolorem.</p>
-            </div>
-            <div class="included-container">
-                <h6>Cancel Anytime</h6>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, dolorem.</p>
-            </div>
-            <div class="included-container">
-                <h6>Satisfaction Guaranteed</h6>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, dolorem.</p>
-            </div>
-        </div>
-        <div class="description-container">
-            <p>
-                {{ product.description }}
-            </p>
+  <div v-if="product" class="container py-5">
+    <div class="hero-container">
+        <img :src="require(`@/assets/images/${product.image}`)" alt="" class="image">
+        <div class="info-box">
+            <h1>{{ product.title }}</h1>
+            <p class="snippet">{{ product.snippet }}</p>
         </div>
     </div>
-    <div v-else class="container padding">
-        Page not found
+    <div class="whats-included-container">
+        <div class="included-container">
+            <h6>Super Effective</h6>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, dolorem.</p>
+        </div>
+        <div class="included-container">
+            <h6>Clean & Tidy</h6>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, dolorem.</p>
+        </div>
+        <div class="included-container">
+            <h6>Cancel Anytime</h6>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, dolorem.</p>
+        </div>
+        <div class="included-container">
+            <h6>Satisfaction Guaranteed</h6>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, dolorem.</p>
+        </div>
     </div>
+    <div class="description-container">
+        <p>
+            {{ product.description }}
+        </p>
+    </div>
+</div>
+<div v-else class="container padding">
+    Page not found
+</div>
 </template>
 
 <script>
