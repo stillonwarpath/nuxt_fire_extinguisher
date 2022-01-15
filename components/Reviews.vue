fac9a8c3dba8a428c3a8825610d96e71dd902a88
francisco <fr.betancourt@live.com>
2022-01-15 19:36:29 -0300

    Fetch users reviews.

--- components/Reviews.vue
+++ components/Reviews.vue
@@ -1,9 +1,25 @@
 <template>
-  <div></div>
+  <div>
+      <h3>Customer Reviews</h3>
+      <div>
+        {{ reviewers }}
+      </div>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      reviewers: []
+    }
+  },
+  async fetch () {
+    this.reviewers = await fetch(
+      'https://randomuser.me/api/?results=5'
+    ).then(res => res.json())
+  }
+}
 </script>
 
 <style scoped></style>
