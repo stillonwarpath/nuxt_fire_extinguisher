19c6e7940ea3e8ee68495e68559e7376bd05a5b8
francisco <fr.betancourt@live.com>
2022-01-14 21:54:49 -0300

    Added layout without a navbar.

---
+++ layouts/no-nav.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <NuxtLink to="/">Go Back</NuxtLink>>
+        <Nuxt />
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
