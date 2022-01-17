189fb8f84778ca9952a51370db312f89243d96a6
francisco <fr.betancourt@live.com>
2022-01-17 11:38:03 -0300

    Added rent modal component to product page.

--- components/RentModal.vue
+++ components/RentModal.vue
@@ -14,19 +14,19 @@
 </template>
 
 <script>
-  export default {
-    methods: {
-      showModal() {
-        this.$refs['my-modal'].show()
-      },
-      hideModal() {
-        this.$refs['my-modal'].hide()
-      },
-      toggleModal() {
-        // We pass the ID of the button that we want to return focus to
-        // when the modal has hidden
-        this.$refs['my-modal'].toggle('#toggle-btn')
-      }
+export default {
+  methods: {
+    showModal () {
+      this.$refs['my-modal'].show()
+    },
+    hideModal () {
+      this.$refs['my-modal'].hide()
+    },
+    toggleModal () {
+      // We pass the ID of the button that we want to return focus to
+      // when the modal has hidden
+      this.$refs['my-modal'].toggle('#toggle-btn')
     }
   }
+}
 </script>
